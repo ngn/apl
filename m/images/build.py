a391210339bd6d57a829a6aacacce81f8e379be8
ngn <nick.nickolov@gmail.com>
2012-09-04 19:03:11 +0300

    a little circle to indicate when caps lock is on

--- m/images/build.py
+++ m/images/build.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 
 from subprocess import call
-from PIL import Image
+#from PIL import Image
 
-# size of an icon
-w = h = 18
+## size of an icon
+#w = h = 18
 
-call(['inkscape', 'symbols.svg', '-e', 'symbols.png'])
-im = Image.open('symbols.png')
-cols = im.size[0] // w
-rows = im.size[1] // h
+#call(['inkscape', 'symbols.svg', '-e', 'symbols.png'])
+#im = Image.open('symbols.png')
+#cols = im.size[0] // w
+#rows = im.size[1] // h
 
-for i in range(rows * cols):
-    r = i // cols
-    c = i % cols
-    x0 = c * w
-    y0 = r * h
-    x1 = x0 + w
-    y1 = y0 + h
-    im.crop((x0, y0, x1, y1)).save('apl%d.png' % i, 'PNG')
+#for i in range(rows * cols):
+#    r = i // cols
+#    c = i % cols
+#    x0 = c * w
+#    y0 = r * h
+#    x1 = x0 + w
+#    y1 = y0 + h
+#    im.crop((x0, y0, x1, y1)).save('apl%d.png' % i, 'PNG')
 
-with open('symbols.css', 'w') as f:
-    for i in range(rows * cols):
-        f.write('.ui-icon-apl%d { background-image: url("apl%d.png"); }\n' % (i, i))
+#with open('symbols.css', 'w') as f:
+#    for i in range(rows * cols):
+#        f.write('.ui-icon-apl%d { background-image: url("apl%d.png"); }\n' % (i, i))
 
-# Export cursor
 call(['inkscape', 'cursor.svg', '-e', 'cursor.png'])
+call(['inkscape', 'button-lock-light.svg', '-e', 'button-lock-light.png'])
