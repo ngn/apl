7c4f1a62a4d6624e5c9dd04534831acc777aed2c
ngn <nick.nickolov@gmail.com>
2013-03-11 14:02:11 +0200

    fake-require.js: renamed "instance" -> "exports"

--- web/fake-require.js
+++ web/fake-require.js
@@ -9,17 +9,17 @@
         }
         if (m.state === 'unloaded') {
             m.state = 'beingloaded';
-            m.instance = m.init(m.instance, window.require);
+            m.exports = m.init(m.exports, window.require);
             m.state = 'loaded';
         }
-        return m.instance;
+        return m.exports;
     };
 
     window.defModule = function (name, init) {
         if (modules[name]) {
             throw Error('Redefinition of module ' + JSON.stringify(name));
         }
-        modules[name] = {state: 'unloaded', init: init, instance: {}};
+        modules[name] = {state: 'unloaded', init: init, exports: {}};
     };
 
 })();
